94bfbc3f94b6c99c1638bdd56c8418ef6073992b
s4ng <leesg951201@gmail.com>
2026-07-29 11:11:37 +0900

    config: use four-space indentation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- This file loads after LazyVim's defaults, which set two-space indentation.
+-- Insert a Tab as four spaces unless a filetype later overrides it locally.
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
